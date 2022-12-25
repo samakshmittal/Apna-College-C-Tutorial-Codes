2a5e98d97b2649a9f0ab0f864f398abafc84d6cd
samakshmittal <samakshmittal826@gmail.com>
2022-12-25 23:20:31 +0530

    Update average.c

--- average.c
+++ average.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-    int a, b, c;
+    int m, b, c;
     printf("Enter a, b, c");
-    scanf("%d", &a);
+    scanf("%d", &m);
     scanf("%d", &b);
     scanf("%d", &c);
-    printf("%d", (a+b+c)/3);
+    printf("%d", (m+b+c)/3);
     return 0;
 }
