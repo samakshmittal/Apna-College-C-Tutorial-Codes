d4ece8c26701f8da338bc0b7a8b53b628eb16e2c
samakshmittal <samakshmittal826@gmail.com>
2022-12-26 00:55:43 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,10 @@
+#include <Stdio.h>
+int main(){
+    int n;
+    printf("Enter number");
+    scanf("%d", &n);
+    for(int i=1; i<=10; ++i){
+        printf("%d*%d=%d\n", n, i, n*i);
+    }
+    return 0;
+}
