d6a0d8bc5790f560fb6af7c913bee52a068a79f2
samakshmittal <samakshmittal826@gmail.com>
2022-12-25 23:19:00 +0530

    Update areacircle.c

--- areacircle.c
+++ areacircle.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int r;
+    int ra;
     printf("Enter radius");
-    scanf("%d", &r);
-    printf("%f", (M_PI)*r*r);
+    scanf("%d", &ra);
+    printf("%f", (M_PI)*ra*ra);
     return 0;
 }
