43e01b640d2f9eab08976ecdfad12da84ef5783b
samakshmittal <samakshmittal826@gmail.com>
2022-12-25 23:27:07 +0530

    Update studentpassfail.c

--- studentpassfail.c
+++ studentpassfail.c
@@ -3,6 +3,7 @@ int main(){
     int n;
     printf("Enter marks:");
     scanf("%d", &n);
+    //ternary
     n>30 && n<=100 ? printf("Pass") : (n<=30 && n>=0 ? printf("Fail") : printf("Invalid input"));
     return 0;
 }
