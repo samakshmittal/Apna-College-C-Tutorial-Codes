b182bce0e0083b62e65a6c7a3b337554981609ba
samakshmittal <samakshmittal826@gmail.com>
2022-12-27 01:40:38 +0530

    Update function4.c

--- function4.c
+++ function4.c
@@ -7,7 +7,7 @@ int main(){
     scanf("%f", &a);
     scanf("%f", &b);
     scanf("%f", &c);
-    printf("Percentage = %f %", average(a, b, c));
+    printf("Percentage = %f", average(a, b, c));
     return 0;
 }
 float average(float science, float maths, float sanskrit){
