ec35467162292dbd2ebc84ebc3cb67ead3d60f48
samakshmittal <samakshmittal826@gmail.com>
2022-12-27 01:33:33 +0530

    Create function4.c

---
+++ function4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+float average(float science, float maths, float sanskrit);
+int main(){
+    float a, b, c;
+    printf("Enter marks");
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    printf("Percentage = %f %", average(a, b, c));
+    return 0;
+}
+float average(float science, float maths, float sanskrit){
+    return (science+maths+sanskrit)/3;
+}
