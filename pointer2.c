a2e8bc7ac09c3e7263a2752e7872e833910f3956
samakshmittal <samakshmittal826@gmail.com>
2022-12-31 02:27:30 +0530

    Create pointer2.c

---
+++ pointer2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void dowork(int a, int b, int *sum, int *prod, int *avg){
+    *sum=a+b;
+    *prod=a*b;
+    *avg=(a+b)/2;
+}
+int main(){
+    int a=3, b=5;
+    int sum, prod, avg;
+    dowork(a, b, &sum, &prod, &avg);
+    printf("sum=%d, prod=%d, avg=%d", sum, prod, avg);
+    return 0;
+}
