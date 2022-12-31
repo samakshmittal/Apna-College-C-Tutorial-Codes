2ad1756c8f0f07f42e28c146bf99d2d0b8062a1d
samakshmittal <samakshmittal826@gmail.com>
2022-12-31 17:27:58 +0530

    Update array.c

--- array.c
+++ array.c
@@ -5,8 +5,8 @@ int main(){
     scanf("%d", &price[0]);
     scanf("%d", &price[1]);
     scanf("%d", &price[2]);
-    printf("total price 1 = %d\n", price[0]+price[0]*0.18);
-    printf("total price 2 = %d\n", price[1]+price[1]*0.18);
-    printf("total price 3 = %d\n", price[2]+price[2]*0.18);
+    printf("total price 1 = %f\n", price[0]+price[0]*0.18);
+    printf("total price 2 = %f\n", price[1]+price[1]*0.18);
+    printf("total price 3 = %f\n", price[2]+price[2]*0.18);
     return 0;
 }
