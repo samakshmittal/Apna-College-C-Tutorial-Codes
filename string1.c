a1e24a56d71e71eb79ad06cf5d3deb5d617d4f99
samakshmittal <samakshmittal826@gmail.com>
2023-01-01 03:22:02 +0530

    Create string1.c

---
+++ string1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char firstname[100];
+    printf("Enter you first name");
+    scanf("%s", firstname);
+    printf("My name is %s", firstname);
+    return 0;
+}
