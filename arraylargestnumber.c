28128f8186722e68a1c5c61734026bfdf36e07ee
samakshmittal <samakshmittal826@gmail.com>
2023-01-01 05:48:06 +0530

    Update arraylargestnumber.c

--- arraylargestnumber.c
+++ arraylargestnumber.c
@@ -12,7 +12,7 @@ int main(){
         for(int j=0; j<n; j++){
             if (arr[i]<arr[j]){
                 x=1;
-                return;
+                break;
             }
             else {
                 x=0;
